638d4a835a5255008cb1a195602945636655b3e8
rellzdorseo <jadorsey@syr.edu>
2026-02-22 21:20:23 -0500

    8

--- project1/script.js
+++ project1/script.js
@@ -3,8 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     M.Tabs.init(document.querySelectorAll('.tabs'));
     M.Collapsible.init(document.querySelectorAll('.collapsible'));
     M.ScrollSpy.init(document.querySelectorAll('.scrollspy'));
-
+  
+    // NEW: mobile nav + hero image effect + optional image zoom
+    M.Sidenav.init(document.querySelectorAll('.sidenav'));
+    M.Parallax.init(document.querySelectorAll('.parallax'));
+    M.Materialbox.init(document.querySelectorAll('.materialboxed'));
+  
     setTimeout(function() {
-        document.getElementById('loader').style.display = "none";
+      document.getElementById('loader').style.display = "none";
     }, 800);
-});
+  });
